61ac25a4cbcbcf3be2c4e8789dd283a6ce78ad7f
Anant-Gulia <64538832+Anant-Gulia@users.noreply.github.com>
2021-05-27 20:46:31 +0530

    Enabled CORS

--- src/main/java/broker/app/controller/BrokerServerController.java
+++ src/main/java/broker/app/controller/BrokerServerController.java
@@ -1,6 +1,7 @@
 package broker.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,27 +24,32 @@ public class BrokerServerController {
 
 	@Autowired
 	private BrokerServerService brokerServerService;
-
+	
+	@CrossOrigin
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
 	public LoginResponse login(@RequestBody LoginRequest request) {
 		return brokerServerService.loginUser(request);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path = "/logout", method = RequestMethod.POST)
 	public LogoutResponse logout(@RequestBody LogoutRequest request) {
 		return brokerServerService.logoutUser(request);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path = "/CreateAccount", method = RequestMethod.POST)
 	public CreateAccountResponse create(@RequestBody CreateAccountRequest request) {
 		return brokerServerService.createAccount(request);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path = "/GetHoldings", method = RequestMethod.POST)
 	public GetHoldingsResponse getHoldings(@RequestBody GetHoldingsRequest request) {
 		return brokerServerService.getUserHoldings(request);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path = "/UpdateHoldings", method = RequestMethod.POST)
 	public UpdateHoldingsResponse updateHoldings(@RequestBody UpdateHoldingsRequest request) {
 		return brokerServerService.updateUserHoldings(request);
